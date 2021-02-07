49db177a9baf02e4f93975225af3d623964da734
mgm63 <mgm63@delmar.umsl.edu>
2021-02-07 11:29:29 -0600

    commit

--- main.c
+++ main.c
@@ -6,27 +6,20 @@
 #include "traversals.h"
 #include "tree.h"
 
-/*struct Node *new_node(char *d) {
-	struct Node *root;
-	root = malloc(sizeof(struct Node));
-	root -> data = d;
-	root -> left = NULL;
-	root -> right = NULL;
-
-	return root;
-}*/
-
 int main(int argc, char *argv[]) {
-	//int a = 'h';
-	//int b = 'a';
+	if (argc == 1) {
+		printf("Number of arguments is %d\n", argc-1);
+		printf("Please call this process w/ at least 1 string file\n");
+		return EXIT_SUCCESS;
+	}
 	char x[30] = "hello";
 	printf("I'm working\n");
 	struct Node *root;
 	root = new_node("hh");
 	printf("%s\n", root -> data);
 	preorder(root);
-	printf("Fuck you\n");
 	insert_node(root, x);
+	postorder(root);
 	//printf("%d\n", a + b);
 	free(root);
 	return 0;
--- tree.c
+++ tree.c
@@ -13,20 +13,26 @@ struct Node* new_node(char* x) {
 }
 
 struct Node* insert_node(struct Node* root, char* x) {
+	
+	//Initial value check
 	if (root == NULL)
 		return new_node(x);
-	printf("Another fuck you\n");
+	//Var declerations
 	int sum_of_x;
 	int sum_of_root;
+	
+	//If the string is long
 	if (strlen(x) >= 2)
 		sum_of_x = x[0] + x[1];
+	//If the string is a single char
 	else if (strlen(x) < 2)
 		sum_of_x = x[0];
+	// Same as above, but for the root data
 	if (strlen(root -> data) >= 2)
 		sum_of_root = root -> data[0] + root -> data[1];
-	if (strlen(root -> data) < 2)
+	else if (strlen(root -> data) < 2)
 		sum_of_root = root -> data[0];
-	else if (sum_of_x > sum_of_root)
+	if (sum_of_x > sum_of_root)
 		root -> right = insert_node(root -> right, x);
 	else
 		root -> left = insert_node(root -> left, x);
